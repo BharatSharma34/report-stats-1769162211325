5260d200e0600e20f13372153378758081355d8d
BharatSharma34 <bharatsharma@ducktaleit.com>
2026-01-23 15:26:53 +0530

    Create frontend/next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/apps/report-stats-1769162211325',
+  publicRuntimeConfig: {
+    basePath: '/apps/report-stats-1769162211325'
+  }
+};
+module.exports = nextConfig;
